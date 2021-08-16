7208947204946dc20e308101eb68b5a2339e272f
Rafael Giro <me@rafaelgiro.dev>
2021-08-16 19:48:41 -0300

    feat: criação, encerramento e visualização de pedidos

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { routerUser, routerPizza, routerProduct, routerCategory, routerMenu } from './router';
+import { routerUser, routerPizza, routerProduct, routerCategory, routerMenu, routerOrder } from './router';
 import { errorHandling } from './middlewares/error';
 
 const defaultPort = 3000;
@@ -12,6 +12,7 @@ app.use('/pizza', routerPizza);
 app.use('/product', routerProduct);
 app.use('/category', routerCategory);
 app.use('/menu', routerMenu);
+app.use('/order', routerOrder);
 
 // Middleware de erros pras rotas
 app.use(errorHandling);
--- src/router/index.ts
+++ src/router/index.ts
@@ -3,5 +3,6 @@ import routerPizza from './pizza.routes';
 import routerProduct from './product.routes';
 import routerCategory from './category.routes';
 import routerMenu from './menu.routes';
+import routerOrder from './order.routes';
 
-export { routerUser, routerPizza, routerProduct, routerCategory, routerMenu };
+export { routerUser, routerPizza, routerProduct, routerCategory, routerMenu, routerOrder };
